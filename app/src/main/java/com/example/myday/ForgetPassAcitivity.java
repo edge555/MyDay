5b7062b0651c0fa55a9bc48c660fba2348f3df16
Shoaib Ahmed <2010.ahmed.shoaib@gmail.com>
2019-08-18 03:17:39 +0600

    Updated

---
+++ app/src/main/java/com/example/myday/ForgetPassAcitivity.java
@@ -0,0 +1,14 @@
+package com.example.myday;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgetPassAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_pass_acitivity);
+    }
+}
