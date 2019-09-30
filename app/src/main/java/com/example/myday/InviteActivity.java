f230746a656b0e66b6d9bda9a29b0fd1d3c7b908
Shoaib Ahmed <2010.ahmed.shoaib@gmail.com>
2019-09-30 17:45:36 +0600

    Added Clipboard Copying

--- app/src/main/java/com/example/myday/InviteActivity.java
+++ app/src/main/java/com/example/myday/InviteActivity.java
@@ -2,13 +2,34 @@ package com.example.myday;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class InviteActivity extends AppCompatActivity {
-
+    private Button bt;
+    private TextView tv;
+    private ClipboardManager myClipboard;
+    private ClipData myClip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invite);
+        myClipboard = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
+        bt = findViewById(R.id.invitecopy);
+        tv = findViewById(R.id.invitelink);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = tv.getText().toString();
+                myClip = ClipData.newPlainText("text", text);
+                myClipboard.setPrimaryClip(myClip);
+                Toast.makeText(getApplicationContext(), "Text Copied",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
