d90df86f5d0bb0d1432d04275cbad1821196ab22
Shoaib Ahmed <2010.ahmed.shoaib@gmail.com>
2019-10-14 10:49:33 +0600

    Updated

--- app/src/main/java/com/example/myday/UpdateActivity.java
+++ app/src/main/java/com/example/myday/UpdateActivity.java
@@ -2,13 +2,38 @@ package com.example.myday;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.github.ybq.android.spinkit.style.DoubleBounce;
+import com.github.ybq.android.spinkit.style.Wave;
 
 public class UpdateActivity extends AppCompatActivity {
 
+    TextView updatetext;
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
+        progressDialog = new ProgressDialog(UpdateActivity.this);
+        progressDialog.setTitle("Update");
+        progressDialog.setMessage("Checking for updates...");
+        progressDialog.show();
+        Runnable progressRunnable = new Runnable() {
+            @Override
+            public void run() {
+                progressDialog.dismiss();
+                updatetext = findViewById(R.id.updatetext);
+                updatetext.setText("You are up to date! Check again later.");
+            }
+        };
+        Handler pdCanceller = new Handler();
+        pdCanceller.postDelayed(progressRunnable, 2500);
     }
 }
